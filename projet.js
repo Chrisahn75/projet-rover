b2c039a7efe07ef8eca99d124ef53cadd583baeb
jess <jessica.elessa@gmail.com>
2022-01-13 09:58:20 +0100

    function move forward

--- projet.js
+++ projet.js
@@ -1,7 +1,5 @@
 // PROJET ROVER
 
-// 
-
 const grid = [
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -15,12 +13,14 @@ const grid = [
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
-//this dans une classe ou fonction pour faire référence à un élement de ta fonc
 
 let rover= {
     direction: "N",
+    x: 0,
+    y: 0,
 }
-console.log(rover)
+
+grid [0][0] ="N"
 
 console.table(grid)
 
@@ -41,24 +41,53 @@ function turnLeft (rover){
     } 
 }
 
-turnLeft()
+turnLeft("N")
 
 function turnRight (rover){
     switch (rover.direction) {
         case "N" : 
-        rover.direction = "E";
-        break;
+            rover.direction = "E";
+            break;
         case  "E" : 
-        rover.direction = "S";
-        break;
+            rover.direction = "S";
+            break;
         case "S" : 
-        rover.direction ="W";
-        break;
+            rover.direction ="W";
+            break;
         case "W" : 
-        rover.direction ="N";
-        break;        
+            rover.direction ="N";
+            break;        
     } 
 }
 
-turnRight ()
+turnRight ("N")
+
+function moveForward (rover){
+   
+    switch (rover.direction) {
+        case "N" : 
+            if (rover.y === 0){
+                console.log ("You can't move forward");
+              } else {rover.y = rover.y--}
+            break;
+        case  "E" :
+            if (rover.x = grid[0].length-1){
+                console.log ("You can't move forward");            
+            } else {rover.x = rover.x++};
+            break;             
+        case "S" : 
+            if (rover.y === grid[0].length-1){
+                console.log ("You can't move forward")
+            } else {rover.y = rover.y--}
+            break;
+        case "W" : 
+        if (rover.x === 0){
+            console.log ("You can't move forward");
+        }else {rover.x = rover.x--}
+        break;
+    };       
+
+};
+
+moveForward ("E");
 
